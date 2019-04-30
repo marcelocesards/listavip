9fb2502352a75ab61436d2874e509a278c9e8c10
zrimlas <marcelo.c.silva@ericssoninovacao.com.br>
2019-04-30 16:54:01 -0300

    commit inicial do projeto

---
+++ src/main/java/br/marcelo/listavip/listavip/ListavipApplication.java
@@ -0,0 +1,12 @@
+package br.marcelo.listavip.listavip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListavipApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ListavipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/marcelo/listavip/listavip/ListavipApplicationTests.java
@@ -0,0 +1,16 @@
+package br.marcelo.listavip.listavip;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ListavipApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
